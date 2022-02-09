55327f9e65f461600c04822f8121de983342b0ed
the1st-hunter <kajit.konstantin@yandex.ru>
2022-02-10 02:19:21 +0300

    Установка react-routes. Фикс iframe.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,33 +1,22 @@
 import React from 'react';
 import { Menu } from '../Menu/Menu';
-import menu, { TabName } from '../../store/Menu';
 import { observer } from 'mobx-react-lite';
 import { CashflowTab } from '../Cashflow/Cashflow';
 import { CashFlowReality } from '../../store/Cashflow';
 import { Accounts } from '../Accounts/Accounts';
 import './App.css';
 import { AllPopups } from '../Popup/Popup';
+import { Route, Routes  } from 'react-router-dom';
 
 export const App = observer(() => {
-    let tab;
-    switch (menu.tab) {
-        case TabName.planning:
-            tab = <CashflowTab reality={CashFlowReality.plan} />;
-            break;
-        case TabName.cashflow:
-            tab = <CashflowTab reality={CashFlowReality.reality} />;
-            break;
-        case TabName.accounts:
-            tab = <Accounts />;
-            break;
-        default:
-            break;
-    }
-
     return (
         <div className='App'>
             <Menu />
-            {tab}
+            <Routes>
+                <Route path='/cashflow' element={<CashflowTab reality={CashFlowReality.reality} />} />
+                <Route path='/planning' element={<CashflowTab reality={CashFlowReality.plan} />} />
+                <Route path='/accounts' element={<Accounts />} />
+            </Routes>
             <AllPopups />
         </div>
     )
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { bem } from '../../utils/bem';
-import menu, { TabName } from '../../store/Menu';
-import { observer } from 'mobx-react-lite';
 import './Menu.css';
+import { Link } from 'react-router-dom';
 
-export const Menu = observer(() => {
+export const Menu = () => {
     const className = bem('menu');
 
-    const buttonClass = (tabName: TabName) => tabName === menu.tab ? className('button', {active: ''}) : className('button');
+    const defaultState = useMemo(() => ({
+        cashflow: className('button'),
+        planning: className('button'),
+        accounts: className('button')
+    }), []);
+
+    const [classNames, setClassNames] = useState(defaultState);
+    
+    const onClick = useCallback((name: keyof typeof defaultState) => {
+        setClassNames({
+            ...defaultState,
+            [name]: className('button', {active: ''})
+        });
+    }, []);
+
+    useEffect(() => {
+        const path = window.location.pathname.split('/')[1];
+        setClassNames({
+            ...defaultState,
+            [path]: className('button', {active: ''})
+        });
+    }, []);
 
     return (        
         <div className={className()}>
-            <div className={buttonClass(TabName.cashflow)} onClick={() => menu.setTab(TabName.cashflow)}>Кешфлоу</div>
-            <div className={buttonClass(TabName.planning)} onClick={() => menu.setTab(TabName.planning)}>Планирование</div>
-            <div className={buttonClass(TabName.accounts)} onClick={() => menu.setTab(TabName.accounts)}>Счета</div>
+            <Link to='/cashflow' className={classNames.cashflow} onClick={() => onClick('cashflow')}>Кешфлоу</Link>
+            <Link to='/planning' className={classNames.planning} onClick={() => onClick('planning')}>Планирование</Link>
+            <Link to='/accounts' className={classNames.accounts} onClick={() => onClick('accounts')}>Счета</Link>
         </div>
     );
-});
+};
